8c3ad79f170e2f78931445693a604bf58a84e367
Andrii <android.kaplan@gmail.com>
2023-01-01 18:41:20 +0200

    initial commit

---
+++ src/script/index.js
@@ -0,0 +1,4 @@
+import "../style/style.scss";
+import * as bootstrap from 'bootstrap';
+import * as fontello from '../icons/fontello-d81d4521/css/fontello.css';
+//import * as functions from "./functions.js";
