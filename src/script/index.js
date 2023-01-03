8a63fb900b06f7633ff2e96d3c73d6dcf55e9b5a
Andrii <android.kaplan@gmail.com>
2023-01-03 22:22:03 +0200

    update layout

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,9 @@
 import "../style/style.scss";
 import * as bootstrap from 'bootstrap';
 import * as fontello from '../icons/fontello-d81d4521/css/fontello.css';
-//import * as functions from "./functions.js";
+import * as functions from "./functions.js";
+
+functions.burgerClick();
+
+// document.querySelector('.footer__logo_column__subtitle').style.maxWidth = document.querySelector('.footer__logo_column ').clientWidth+'px';
+// document.querySelector('.footer__contact_us_column__subtitle').style.maxWidth = document.querySelector('.footer__contact_us_column').clientWidth+'px';
