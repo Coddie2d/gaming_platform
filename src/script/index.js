4bcd7052b9c02a1259a3b31303b98f216b54e27f
Andrii <android.kaplan@gmail.com>
2023-01-04 21:02:28 +0200

    update layout

--- src/script/index.js
+++ src/script/index.js
@@ -2,6 +2,7 @@ import "../style/style.scss";
 import * as bootstrap from 'bootstrap';
 import * as fontello from '../icons/fontello-d81d4521/css/fontello.css';
 import * as functions from "./functions.js";
+import $ from 'jquery';
 
 functions.burgerClick();
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const loader = require('sass-loader');
 require('graceful-fs').gracefulify(require('fs'));
 
-const pages = ["index", ];
+const pages = ["index","about_us"];
 
 module.exports = {
     
